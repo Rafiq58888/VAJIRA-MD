bb4730191982aef5e1e48bc99c21d6d7b0c77add
Rafiq58888 <rafiq0000012a@gmail.com>
2024-07-01 08:52:08 +0500

    Update settings.js
    Done

--- settings.js
+++ settings.js
@@ -8,10 +8,10 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '923405452658' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=KEtjjTqR#qWd8Sb5OMw3GmAQ0nsD9l3DZ6nAc9jpi0o54W4P6ZSVAJIRA-MD=KEtjjTqR#qWd8Sb5OMw3GmAQ0nsD9l3DZ6nAc9jpi0o54W4P6ZSw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
